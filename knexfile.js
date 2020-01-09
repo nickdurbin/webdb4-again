a4450ecdbb2c4a7d33d6ab2aac6da8b4974c25d1
ndurbin <nick.durbin@gmail.com>
2020-01-09 17:02:02 -0500

    Added the migration and seeding to the knex file

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,12 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);

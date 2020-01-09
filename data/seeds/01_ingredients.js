dd1337f7ff07526c5028adb44fd60c0c24ab9c0c
ndurbin <nick.durbin@gmail.com>
2020-01-09 17:03:08 -0500

    Initial seed data for ingredients.

---
+++ data/seeds/01_ingredients.js
@@ -0,0 +1,9 @@
+exports.seed = function(knex) {
+  return knex('ingredients')
+    .then(function () {
+      return knex('ingredients').insert([
+        {id: 1, ingredient_name: 'Noodles'},
+        {id: 2, ingredient_name: 'Orange'},
+      ]);
+    });
+};

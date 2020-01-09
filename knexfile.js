04e2368d05bf7a87e8eef193844c8f6953750814
ndurbin <nick.durbin@gmail.com>
2020-01-09 03:08:40 -0500

    Initialized knex and boiler plate code.

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+ 
+};

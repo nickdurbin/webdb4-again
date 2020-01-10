5f910e561a5ef88c8de5ec9ba6d9b8aa3e5fade4
ndurbin <nick.durbin@gmail.com>
2020-01-10 00:57:16 -0500

    Built out some initial routes for the recipes.

---
+++ api/routes/recipes/recipe-routes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const Recipes = require('./recipe-model')
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+  try {
+    const recipes = await Recipes.getRecipes()
+    console.log(recipes)
+    return res.json(recipes)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const recipe = await Recipes.getRecipeById(id)
+    return res.json(recipe)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post("/", async (req, res, next) => {
+  try {
+    const [id] = await db("recipes").insert(req.body)
+    const newRecipe = await db("recipes").where('id', id).first()
+    return res.status(201).json(newRecipe)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put("/:id", (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete("/:id", (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router

33ced107399a99dd4ba564636446c13a4b4170ce
ndurbin <nick.durbin@gmail.com>
2020-01-10 00:56:33 -0500

    Removed the ingredients routes for now.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,5 @@
-const ingredientRoutes = require('./ingredients/ingredient-routes')
 const recipeRoutes = require('./recipes/recipe-routes')
 
 module.exports = server => {
-  server.use("/api/ingredients", ingredientRoutes)
   server.use("/api/recipes", recipeRoutes)
 }

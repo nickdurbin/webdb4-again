db887a6d67fa9ea49d2af6cf4e63d6d18d716794
ndurbin <nick.durbin@gmail.com>
2020-01-10 00:57:03 -0500

    Added the helper functions for the model.

---
+++ api/routes/recipes/recipe-model.js
@@ -0,0 +1,45 @@
+const db = require('../../../data/db-config')
+
+async function getInstructions(id) {
+  return await db('recipes').select('instructions').where({ id: recipe_id }).first()
+}
+
+async function getRecipes() {
+  return await db('recipes')
+}
+
+async function getRecipeById(id) {
+  return await db('recipes').where({ id }).first()
+}
+
+async function getShoppingCart(id) {
+  return await db('shopping_cart').where({ id }).first()
+}
+
+async function removeRecipe(id) {
+  return await db('recipes')
+  .where({ id })
+  .del()
+}
+
+async function removeIngredient(id) {
+  return await db('ingredients')
+  .where({ id })
+  .del()
+}
+
+async function removeShoppingCart(id) {
+  return await db('shopping_cart')
+  .where({ id })
+  .del()
+}
+
+module.exports = {
+  getInstructions,
+  getRecipes,
+  getRecipeById,
+  getShoppingCart,
+  removeRecipe,
+  removeIngredient,
+  removeShoppingCart
+}
